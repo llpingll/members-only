5ea66e9ffc3d961173429f14180609016222a77b
llpingll <94099281+llpingll@users.noreply.github.com>
2024-07-10 21:07:40 +0100

    Add connection to database & only allow server connection if database connection successful

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const sassMiddleware = require("node-sass-middleware");
 require("dotenv").config();
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -30,7 +29,6 @@ app.use(
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- db.js
+++ db.js
@@ -7,3 +7,15 @@ if (process.env.NODE_ENV !== "production") {
 
 // Get the database connection string
 const DB = process.env.DATABASE_URI;
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(DB);
+    console.log("Connected to database");
+  } catch (error) {
+    console.log("Failed to connect to database", error);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectToDB;
